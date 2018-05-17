c6800868e712952262e082da2d393f7f6cd6352d
tstillwell2k17 <tristanstillwell@gmail.com>
2018-05-16 21:30:19 -0400

    feat: Use flatten function instead of concat/spread

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ function getRandom (equations, weights) {
 
 function maxPoint(points) {
   // return largest value from points
-  let point_values = [].concat(...points);
+  let point_values = flatten(points);
   let max = points_values.reduce(function(a,b) {
     return Math.max(a, b);
   });
