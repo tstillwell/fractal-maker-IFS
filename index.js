60fe5bbc1bc0b1e6d4abfb0c1ce7a1f6caff4a19
tstillwell2k17 <tristanstillwell@gmail.com>
2018-05-19 22:04:10 -0400

    refactor: Convert equations to numbers at push time

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function promptForIFS(){
   switch (isValidEquation(line)) {
     case true:
       console.log('valid input!');
-      equations.push(line.split(" "));
+      equations.push(Number(line.split(" ")));
       break;
     case false:
       if (line == ""){
@@ -77,8 +77,8 @@ function generateNextPoint(point, IFSparams){
   // if input point is (x,y)
   // and IFS params are a b c d e f
   // next point is (ax+by+e, cx+dy+f)
-  let next_point_x = Number(IFSparams[0]) * point[0] + Number(IFSparams[1]) * point[1] + Number(IFSparams[4]);
-  let next_point_y = Number(IFSparams[2]) * point[0] + Number(IFSparams[3]) * point[1] + Number(IFSparams[5]);
+  let next_point_x = IFSparams[0] * point[0] + IFSparams[1] * point[1] + IFSparams[4];
+  let next_point_y = IFSparams[2] * point[0] + IFSparams[3] * point[1] + IFSparams[5];
   let next_point = [next_point_x, next_point_y];
   return next_point;
 }
