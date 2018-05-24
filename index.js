feeed9dd64f2fc02b5d2ddc95b37a2d60ea47616
tstillwell2k17 <tristanstillwell@gmail.com>
2018-05-23 22:24:58 -0400

    feat: Get x-scale from scales array

--- index.js
+++ index.js
@@ -144,6 +144,7 @@ function plotPoints(points, width, height){
   let canvas = new Canvas(width, height);
   let ctx = canvas.getContext('2d');
   let scales = scaleFactor(points, width, height);
+  let x_scale = scales[0];
   // plot array of points
   points.forEach(function(point) {
     // convert input (cartesian) coordinates to screen coordinates
